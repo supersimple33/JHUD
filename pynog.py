3a5696929b47b1d477a8b0d54c9b504ca0811ef9
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-11-18 18:45:34 -0500

    4 Spaces Indenting

--- pynog.py
+++ pynog.py
@@ -34,72 +34,72 @@ port = obd_io.OBDPort("/dev/rfcomm0",None,5,0)
 print("Consider Using A Window")
 
 def initCommunication():
-  #call("sudo rfcomm connect 0 00:1D:A5:00:0F:A1 &>/dev/null",shell=True)
-  if port.State == 0:
-    return None
-  active = []
+    #call("sudo rfcomm connect 0 00:1D:A5:00:0F:A1 &>/dev/null",shell=True)
+    if port.State == 0:
+        return None
+    active = []
 
-  supp = port.sensor(0)[1]
-  print("Comm initialized")
-  print(active)
-  print(supp)
-  for i in range(1, len(supp)):
-    if supp[i-1] == "1": #put X in coloum if PID is supported
-      active.append(1)
-    else:
-      active.append(0)
-  #while(1):
-    #for i in range(len(supp)):
-  #  for i in range(50):
-     # if supp[i]=="1":
-  #      try:
-  #        print `((port.sensor(12)[1]-1346500000)-689000)/10.55-700`+"RPM"
-  #      except:
-  #        print "ERROR ERROR"
+    supp = port.sensor(0)[1]
+    print("Comm initialized")
+    print(active)
+    print(supp)
+    for i in range(1, len(supp)):
+        if supp[i-1] == "1": #put X in coloum if PID is supported
+            active.append(1)
+        else:
+            active.append(0)
+    #while(1):
+        #for i in range(len(supp)):
+    #    for i in range(50):
+        # if supp[i]=="1":
+    #            try:
+    #                print `((port.sensor(12)[1]-1346500000)-689000)/10.55-700`+"RPM"
+    #            except:
+    #                print "ERROR ERROR"
 initCommunication()
 
 
 def getRPM():
-  try:
-   # return ((port.sensor(12)[1]-1346500000)-689000)/10.5-700
-    rpms = port.sensor(12)[1]
-    print(rpms)
-    rpms = rpms & 0x0000FFFF
-    print('b')
-    return int(rpms)
-  except:
-    return "err"
+    try:
+     # return ((port.sensor(12)[1]-1346500000)-689000)/10.5-700
+        rpms = port.sensor(12)[1]
+        print(rpms)
+        rpms = rpms & 0x0000FFFF
+        print('b')
+        return int(rpms)
+    except:
+        return "err"
 
 def getSpeed():
-  try:
-    speeds =  port.sensor(13)[1]
-    speeds = int(speeds) 
-    speeds = speeds & 0x000000FF
-    speeds /= 1.609
-    return int(speeds)
-  except:
-    return "Drugs are bad mmmkay?"
+    try:
+        speeds =    port.sensor(13)[1]
+        speeds = int(speeds) 
+        speeds = speeds & 0x000000FF
+        speeds /= 1.609
+        return int(speeds)
+    except:
+        return "Drugs are bad mmmkay?"
 
 def getCoolantTemp():
-  try:
-    return port.sensor(5)[1] & 0x000000FF
-  except:
-    return "err"
+    try:
+        return port.sensor(5)[1] & 0x000000FF
+    except:
+        return "err"
 
 def getIntakeTemp():
-  try:
-    return port.sensor(15)[1] & 0x00000000FF
-  except:
-    return "err"
+    try:
+        return port.sensor(15)[1] & 0x00000000FF
+    except:
+        return "err"
 
 def getThrottle():
-  try:
-    return int((port.sensor(17)[1] & 0x00000000FF)* 100.0 / 255.0)
-  except:
-    return "err"
+    try:
+        return int((port.sensor(17)[1] & 0x00000000FF)* 100.0 / 255.0)
+    except:
+        return "err"
 
 def getLoad():
-  try:
-    return int(( port.sensor(4)[1] & 0x00000000FF)*100.0/255.0)
-  except:
-    return "err"
+    try:
+        return int(( port.sensor(4)[1] & 0x00000000FF)*100.0/255.0)
+    except:
+        return "err"
